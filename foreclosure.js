3e7ae25de246ff474b9c855f49ed0e283be83d77
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-18 21:10:43 -1000

    fail to invoke loan() and assign to stevesloan variable

--- foreclosure.js
+++ foreclosure.js
@@ -6,5 +6,27 @@ var month = 0;
 var monthsUntilEvicted;
 
 function loan() {
-  var account = { borrowed : 550000, balance : 286000, monthlyPayment : 1700, defaulted : 0, defaultsToForeclose : 5, foreclosed : false };
+  var account = {
+    borrowed : 550000,
+    balance : 286000,
+    monthlyPayment : 1700,
+    defaulted : 0,
+    defaultsToForeclose : 5,
+    foreclosed : false
+  };
+
+  function missPayment() {
+    account.defaulted += 1;
+    if (account.defaulted >= account.defaultsToForeclose) {
+      account.foreclosed = true;
+    }
+  }
+
+  return {
+    getBalance : function() {
+    }
+  };
+
 }
+
+loan()
