5d2264162e545f45a9d094acd2644dc5bfc49d39
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-18 20:48:43 -1000

    declared var account {} with key value pairs

--- foreclosure.js
+++ foreclosure.js
@@ -5,6 +5,6 @@ var stevesLoan;
 var month = 0;
 var monthsUntilEvicted;
 
-function loan(){
-
+function loan() {
+  var account = { borrowed : 550000, balance : 286000, monthlyPayment : 1700, defaulted : 0, defaultsToForeclose : 5, foreclosed : false };
 }
